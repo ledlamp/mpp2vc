83daefa95395dfd9ff3a8b45b3d6310862ea12b5
Lamp <ledlamp89@gmail.com>
2018-07-29 15:08:16 -0700

    DEFAULT_VELOCITY is not defined

--- mpp2midiout.js
+++ mpp2midiout.js
@@ -55,7 +55,7 @@ gClient.on("n", function(msg) {
         if(note.s) {
             gMidiOutTest(note.n, 0, ms);
         } else {
-            var vel = (typeof note.v !== "undefined")? parseFloat(note.v) : DEFAULT_VELOCITY;
+            var vel = (typeof note.v !== "undefined")? parseFloat(note.v) : 0.5;
             if(vel < 0) vel = 0; else if (vel > 1) vel = 1;
             gMidiOutTest(note.n, vel * 127, ms);
         }
