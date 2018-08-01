cde021dfbd71cfd1eba80e612d594b5ea1a3ffae
Lamp <ledlamp89@gmail.com>
2018-08-01 13:31:00 -0700

    leave voice on shutdown

--- midiin2discord.js
+++ midiin2discord.js
@@ -1,4 +1,4 @@
-
+var exitHook = require('exit-hook');
 
 var child_process = require("child_process");
 var timidity = child_process.spawn('timidity', ['-iA', '-c', 'timidity.cfg', '-o', '-']);
@@ -6,7 +6,6 @@ timidity.on("error", console.error);
 timidity.stderr.on("data", data => {
     console.log(data.toString());
 });
-var exitHook = require('exit-hook');
 exitHook(function(){
     timidity.kill();
 })
@@ -20,6 +19,9 @@ DiscordBot.on("ready", async function(){
     console.log("Discord Bot Ready");
     var voiceChannel = DiscordBot.channels.get("339628587747639296");
     var voiceConnection = await voiceChannel.join();
+    exitHook(function(){
+        voiceChannel.leave();
+    });
     var dispatcher = voiceConnection.playConvertedStream(timidity.stdout);
     dispatcher.on('end', reason => console.log("dispatcher ended:", reason));
 })
